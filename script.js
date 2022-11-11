9ae1ec915598e622447bb9fd99694d4baddf63f3
Giorgi Tskhvedadze <giorgicxvedadze4@gmail.com>
2022-11-11 16:07:50 +0400

    added the players switching logic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 
 //  Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -17,7 +19,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden')
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
   // 1. Generating a random dice roll
@@ -30,8 +35,12 @@ btnRoll.addEventListener('click', () => {
   // 3. Check for rolled 1: if true, switch to next player
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
   }
 })
